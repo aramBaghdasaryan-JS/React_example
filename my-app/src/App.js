3e9c8e4f5998353279078f6de7061b0d22153ede
Aram Baghdasaryan <arambag.js@gmail.com>
2022-03-27 23:09:49 +0400

    Add PostForm

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,7 @@ import PostList from "./components/PostList";
 import "./styles/app.css"
 import MyButton from "./components/UI/button/MyButton";
 import MyInput from "./components/UI/input/MyInput";
+import PostForm from "./components/UI/PostForm";
 
 function App() {
     const [posts, setPosts] = useState([
@@ -11,36 +12,22 @@ function App() {
         {id: 3, title: "Javascript 3", body: 'Description'},
         {id: 4, title: "Javascript 4", body: 'Description'},
     ])
-    const [post, setPost] = useState({title: '', body: ''})
 
-    const addNewPost = (e) => {
-        e.preventDefault();
+    const createPost = (newPost) => {
+        setPosts([...posts, newPost])
+    }
 
-        setPosts([...posts, {...post, id: Date.now()}])
-        setPost({title: '', body: ''})
-
+    const removePost = (post) => {
+        setPosts(posts.filter(p => p.id !== post.id))
     }
 
 
     return (
         <div className="App">
 
-            <form>
-                <MyInput
-                    value={post.title}
-                    onChange={e => setPost({...post, title: e.target.value})}
-                    type="text"
-                    placeholder="Title"
-                />
-                <MyInput
-                    value={post.body}
-                    onChange={e => setPost({...post, body: e.target.value})}
-                    type="text"
-                    placeholder="Description"
-                />
-                <MyButton onClick={addNewPost}>Create post</MyButton>
-            </form>
-            <PostList posts={posts} title="Posts about JS"/>
+            <PostForm create = {createPost}/>
+
+            <PostList remove={removePost} posts={posts} title="Posts about JS"/>
         </div>
     );
 }
